b9a8aa2a76bc8bf0550dd0a224f62e0efc74e872
ccc <522978139@qq.com>
2017-03-19 15:52:10 +0800

    init commit

---
+++ learn2-1.py
@@ -0,0 +1,8 @@
+print("-------割-----------");
+temp= input("不妨猜一下我想的数字");
+guess = int(temp);
+if guess == 8:
+    print("true,yes,you are right");
+else:
+    print("false,not the number");
+print("end");
